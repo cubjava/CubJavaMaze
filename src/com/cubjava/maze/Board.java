a3e854dd109aa6564a0fc9836530f0c50b8ba3e3
cubjava <cubjava@outlook.com>
2016-11-13 18:57:20 -0500

    11/13

--- src/com/cubjava/maze/Board.java
+++ src/com/cubjava/maze/Board.java
@@ -12,10 +12,10 @@ import javax.swing.JPanel;
 
 public class Board extends JPanel implements ActionListener
 {
-	private Timer timer;
-	private MazeMap m;
-	private Tank t;
-	
+	private Timer	timer;
+	private MazeMap	m;
+	private Tank	t;
+
 	public Board()
 	{
 		m = new MazeMap();
@@ -32,60 +32,68 @@ public class Board extends JPanel implements ActionListener
 		repaint();
 
 	}
-	
+
 	public void paint(Graphics g)
 	{
 		super.paint(g);
-		for(int y = 0; y < 14; y++)
+		for (int y = 0; y < 14; y++)
 		{
-			for(int x = 0; x < 14; x++)
+			for (int x = 0; x < 14; x++)
 			{
-				if(m.getMap(x, y).equals("g"))
+				if (m.getMap(x, y).equals("g"))
 				{
 					g.drawImage(m.getRoad(), x * 32, y * 32, null);
 				}
-				
-				if(m.getMap(x, y).equals("w"))
+
+				if (m.getMap(x, y).equals("w"))
 				{
 					g.drawImage(m.getWall(), x * 32, y * 32, null);
 				}
 			}
-			
-			g.drawImage(t.getTankImage(), t.getX(), t.getY(), null);
+
+			g.drawImage(t.getTankImage(), t.getTileX() * 32, t.getTileY() * 32, null);
 		}
 	}
-	
+
 	public class Al extends KeyAdapter
 	{
 		public void keyPressed(KeyEvent e)
 		{
 			int keycode = e.getKeyCode();
-			if(keycode == KeyEvent.VK_W)
+			if (keycode == KeyEvent.VK_UP)
 			{
-				t.move(0, -32, 0, -1);
+				if (t.getTileY() > 0 &&
+					!m.getMap(t.getTileX(), t.getTileY() - 1).equals("w"))
+					t.move(0, -1);
 			}
-			if(keycode == KeyEvent.VK_S)
+			if (keycode == KeyEvent.VK_DOWN)
 			{
-				t.move(0, 32, 0, 1);
+				if (t.getTileY() < 13 &&
+					!m.getMap(t.getTileX(), t.getTileY() + 1).equals("w"))
+					t.move(0, 1);
 			}
-			if(keycode == KeyEvent.VK_A)
+			if (keycode == KeyEvent.VK_LEFT)
 			{
-				t.move(-32, 0, -1, 0);
+				if (t.getTileX() > 0 &&
+					!m.getMap(t.getTileX() - 1, t.getTileY()).equals("w"))
+					t.move(-1, 0);
 			}
-			if(keycode == KeyEvent.VK_D)
+			if (keycode == KeyEvent.VK_RIGHT)
 			{
-				t.move(32, 0, 1, 0);
+				if (t.getTileX() < 13 
+					&& !m.getMap(t.getTileX() + 1, t.getTileY()).equals("w"))
+					t.move(1, 0);
 			}
 		}
-		
+
 		public void keyReleased(KeyEvent e)
 		{
-			
+
 		}
-		
+
 		public void keyTyped(KeyEvent e)
 		{
-			
+
 		}
 	}
 }
